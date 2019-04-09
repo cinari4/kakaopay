b6ee0d78c3c6bea7d0841895f1e8c5db17808062
ChoiMunGyu <mong2e@ncsoft.com>
2019-04-10 07:44:55 +0900

    add testcases

--- src/main/java/com/kakaopay/apisrv/api/ParseDataApi.java
+++ src/main/java/com/kakaopay/apisrv/api/ParseDataApi.java
@@ -1,14 +1,21 @@
 package com.kakaopay.apisrv.api;
 
 import com.opencsv.CSVReader;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 public class ParseDataApi {
+    /**
+     * csv형식의 데이터를 파싱합니다.
+     *
+     * @param filename
+     * @return csv파싱한 데이터를 (파일을 찾을 수 없거나, IOException이 발생하면 null) 돌려줍니다
+     */
     public List<List<String>> csv(String filename) {
         ClassLoader classLoader = this.getClass().getClassLoader();
         List<List<String>> records = new ArrayList<>();
@@ -18,10 +25,9 @@ public class ParseDataApi {
             while ((values = csvReader.readNext()) != null) {
                 records.add(Arrays.asList(values));
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             return null;
         }
-        
         return records;
     }
 }
--- src/test/java/com/kakaopay/apisrv/api/ParseDataApiTest.java
+++ src/test/java/com/kakaopay/apisrv/api/ParseDataApiTest.java
@@ -1,16 +1,48 @@
 package com.kakaopay.apisrv.api;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ParseDataApiTest {
     @Test
-    public void parse_test() {
+    public void parse_test1_read_exist_file() {
         ParseDataApi parseDataApi = new ParseDataApi();
         List<List<String>> data = parseDataApi.csv("data2.csv");
         assertNotNull(data);
+    }
+
+    @Test
+    public void parse_test1_read_not_exist_file() {
+        ParseDataApi parseDataApi = new ParseDataApi();
+        List<List<String>> data = parseDataApi.csv("data2_not_exist.csv");
+        assertNull(data);
+    }
+
+    @Test
+    public void parse_test2_check_sampledata() {
+        ParseDataApi parseDataApi = new ParseDataApi();
+        List<List<String>> data = parseDataApi.csv("data2.csv");
+        assertNotNull(data);
+
+        // 읽어온 샘플 데이터 사이즈가 111인지
+        assertEquals(111, data.size());
+
+        // 읽어온 샘플 데이터의 각 행의 열 개수가 6인지
+        for (List<String> rowData : data) {
+            assertEquals(6, rowData.size());
+        }
+
+        // 읽어온 샘플 데이터의 첫 행 비교
+        List<String> firstRowData = Arrays.asList("번호", "프로그램명", "테마별 분류", "서비스 지역", "프로그램 소개", "프로그램 상세 소개");
+        assertEquals(firstRowData, data.get(0));
+
+        // 읽어온 샘플 데이터의 끝 행 비교
+        List<String> lastRowData = Arrays.asList("110", "[생태나누리] 아름다운 날들을 위한 힐링", "자연생태체험,", "충청북도 제천시 한수면", "", " 문경지구 실버세대의 건강증진과 여가생활 지원을 결합한 힐링 프로그램입니다.");
+        assertEquals(lastRowData, data.get(data.size()-1));
     }
 }
 
